261f25833bf7b8127b85e563f70645d8e54da533
Sonu Kumar <skbhati199@gmail.com>
2024-01-27 15:39:37 +0530

    intial code

--- App.js
+++ App.js
@@ -1,8 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as  Google from 'expo-auth-session/providers/google';
+import * as WebBrowser from 'expo-web-browser';
+import * as React from 'react';
+
+WebBrowser.maybeCompleteAuthSession();
 
 
 export default function App() {
+
+  const [accessToken, setAccessToken] = React.useState();
+  const [userInfo, setUserInfo] = React.useState();
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    androidClientId: "",
+    iosClientId: "",
+    expoClientId: "",
+  });
+
+  React.useEffect(() => {
+    if (response?.type === 'success') {
+      const { authentication } = response;
+      console.log(authentication);
+    }
+  }, [response]);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
